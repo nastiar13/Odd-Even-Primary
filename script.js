cc551a932624d1af811479d89445c3ce9123c006
nastiar13 <jahatmamang0@gmail.com>
2021-10-22 09:53:08 +0700

    commit

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+const inputNumber = document.querySelector("#input-number")
+const odd = document.querySelector('#odd')
+const even = document.querySelector('#even')
+const primary = document.querySelector('#primary')
+const reset = document.querySelector('#reset')
+const showNumber = document.querySelector('#show-number')
+
+// Prime Number
 const isPrime = ( n ) => {
     for(let i = 2; i <= n/2; i++) {
         if(n % i == 0 && n != 2) {
@@ -20,5 +28,43 @@ const printPrimeNumber = (n) => {
     return arr
 }
 
-let prime = printPrimeNumber(10)
-console.log(prime)
+const printOddNumber = (n) => {
+    let odd = []
+    for(i = 1; i <=n ; i++) {
+        if(i % 2 == 1) {
+            odd.push(i)
+        }
+    }
+    return odd
+}
+
+const printEvenNumber = (n) => {
+    let even = []
+    for( i = 1; i <= n; i++) {
+        if( i % 2 == 0) {
+            even.push(i)
+        }
+    }
+    return even
+}
+
+odd.addEventListener('click', () => {
+    let number = '<h1>Odd Number</h1>'
+    printOddNumber(inputNumber.value).forEach(n => number += `<p>${n}</p>`)
+    showNumber.innerHTML = number
+})
+even.addEventListener('click', () => {
+    let number = '<h1>Even Number</h1>'
+    printEvenNumber(inputNumber.value).forEach(n => number += `<p>${n}</p>`)
+    showNumber.innerHTML = number
+})
+primary.addEventListener('click', () => {
+    let number = '<h1>Prime Number</h1>'
+    printPrimeNumber(inputNumber.value).forEach(n => number += `<p>${n}</p>`)
+    showNumber.innerHTML = number
+})
+reset.addEventListener('click', () => {
+    let number = '<h1>Odd, Even, & Prime</h1>'
+    showNumber.innerHTML = number
+    inputNumber.value = null
+})
